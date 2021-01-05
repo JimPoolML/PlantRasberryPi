470ac3750126e6cc2fd6615dcebd3ffaf4542ccf
Jim Moreno <jim.moreno@olimpiait.com>
2021-01-05 14:49:51 -0500

    implements BarChar Char to show distance

--- app/src/main/java/appjpm4everyone/plantrasberrypi/ui/main/MainActivity.kt
+++ app/src/main/java/appjpm4everyone/plantrasberrypi/ui/main/MainActivity.kt
@@ -5,7 +5,6 @@ import android.database.MatrixCursor
 import android.graphics.Color
 import android.os.Bundle
 import android.provider.BaseColumns
-import android.util.Log
 import androidx.cursoradapter.widget.SimpleCursorAdapter
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -21,9 +20,8 @@ import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.components.LimitLine
 import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.*
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
-import com.github.mikephil.charting.utils.ColorTemplate
 
 
 class MainActivity : BaseActivity() {
@@ -46,8 +44,9 @@ class MainActivity : BaseActivity() {
     private var plotData: Boolean = true
     private var set : ILineDataSet? = null
     private var count : Int = 0
-    private var yValues: ArrayList<Entry> = ArrayList()
+    private var yValues: MutableList<Entry> = mutableListOf()
     private var pieValues: ArrayList<PieEntry> = ArrayList()
+    private var barValues: ArrayList<BarEntry> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -136,18 +135,26 @@ class MainActivity : BaseActivity() {
         upperLimit.labelPosition = LimitLine.LimitLabelPosition.RIGHT_TOP
         upperLimit.textSize = 15f
 
-        var leftAxis = binding.chart.axisLeft
+        val leftAxis = binding.chart.axisLeft
         leftAxis.addLimitLine(upperLimit)
         leftAxis.axisMaximum = 100f
         leftAxis.axisMinimum = 0f
         leftAxis.setDrawLimitLinesBehindData(true)
 
-        var rightAxis = binding.chart.axisRight
+        val rightAxis = binding.chart.axisRight
         rightAxis.addLimitLine(upperLimit)
         rightAxis.axisMaximum = 100f
         rightAxis.axisMinimum = 0f
         rightAxis.setDrawLimitLinesBehindData(true)
+
+        val xAxis = binding.chart.xAxis
+        xAxis.axisMinimum = 0f
+        xAxis.axisMaximum = 20f
+        xAxis.setDrawLimitLinesBehindData(true)
+
         startPlot()
+
+
     }
 
     private fun startPlot() {
@@ -158,7 +165,7 @@ class MainActivity : BaseActivity() {
                     Thread.sleep(1000)
                     addEntryTemp()
                     addEntryHum()
-                    Log.e("Main", "test")
+                    addEntryDistance()
                 }catch (e: InterruptedException){
                     e.printStackTrace()
                 }
@@ -172,30 +179,33 @@ class MainActivity : BaseActivity() {
         val random = (0..100).random()
         yValues.add(Entry(count.toFloat(), random.toFloat()))
 
-        var set1: LineDataSet = LineDataSet(yValues, "Data set 1")
+        val set1: LineDataSet = LineDataSet(yValues, "Data set 1")
         set1.fillAlpha = 110
         set1.color = Color.RED
         set1.lineWidth = 3f
         set1.valueTextSize = 10f
         set1.valueTextColor = R.color.purple_toolbar
 
-        var dataSets: ArrayList<ILineDataSet> = ArrayList()
+        val dataSets: ArrayList<ILineDataSet> = ArrayList()
         dataSets.add(set1)
 
-        var data: LineData = LineData(dataSets)
+        val data: LineData = LineData(dataSets)
         binding.chart.data = data
         binding.chart.invalidate()
 
         if(count>19){
             //Clear data
-            /*val auxValues : ArrayList<Entry> = ArrayList()
+            val auxValues : ArrayList<Entry> = ArrayList()
             yValues.reverse()
             for (i in 0..4) {
                 auxValues.add(Entry(i.toFloat(), yValues[i].y))
-            }*/
-            count=0
-            yValues = ArrayList()
-            //yValues = auxValues
+            }
+            count=5
+            yValues.clear()
+            for(i in auxValues.indices){
+                yValues.add(auxValues[i])
+            }
+            yValues = auxValues
         }else{
             count += 1
         }
@@ -233,75 +243,79 @@ class MainActivity : BaseActivity() {
         binding.chartPie.description = description
 
 
-        var pieDataSet : PieDataSet = PieDataSet(pieValues, "")
+        val pieDataSet : PieDataSet = PieDataSet(pieValues, "")
         pieDataSet.sliceSpace = 1f
         pieDataSet.selectionShift = 5f
 
         // add a lot of colors
         val colors = ArrayList<Int>()
-        colors.add(Color.BLUE)
+        if(random<=400f){
+            colors.add(Color.BLUE)
+        }else{
+            colors.add(Color.GREEN)
+        }
         colors.add(Color.LTGRAY)
         pieDataSet.colors = colors
 
         //Add Data
-        var pieData: PieData = PieData(pieDataSet)
+        val pieData: PieData = PieData(pieDataSet)
         //0f equals to hide :)
         pieData.setValueTextSize(0f)
         pieData.setValueTextColor(Color.WHITE)
 
         binding.chartPie.data = pieData
         binding.chartPie.invalidate()
-
     }
 
+    private fun addEntryDistance(){
+        binding.chartBar.isDragXEnabled = true
+        binding.chartBar.setScaleEnabled(false)
+        binding.chartBar.description.text = "Distance cm Data"
+        binding.chart.setBackgroundColor(Color.WHITE)
+
+        val labels: ArrayList<String> = ArrayList()
+        labels.add("cm")
+        val xAxis = binding.chartBar.xAxis
+        xAxis.textColor = R.color.purple_toolbar
+        xAxis.textSize = 15f
+        xAxis.valueFormatter = IndexAxisValueFormatter(labels)
+
+        val axisLeft: YAxis = binding.chartBar.axisLeft
+        axisLeft.granularity = 2f
+        axisLeft.axisMinimum = 0f
+        axisLeft.axisMaximum= 400f
+
+        val axisRight: YAxis = binding.chartBar.axisRight
+        axisRight.granularity = 2f
+        axisRight.axisMinimum = 0f
+        axisRight.axisMaximum= 400f
+
+        //Simulate de distance sensor
+        val random = (0..400).random().toFloat()
+        barValues = ArrayList()
+        barValues.add(BarEntry(5f, random))
 
 
-    private fun createSet(): ILineDataSet? {
-        var set: LineDataSet = LineDataSet(null, "Data set 1")
-        set.axisDependency = YAxis.AxisDependency.LEFT
-        set.lineWidth = 3F
-        set.color = Color.RED
-        set.mode = LineDataSet.Mode.CUBIC_BEZIER
-        set.cubicIntensity = 0.2f
-        return set
-    }
+        val barDataSet: BarDataSet = BarDataSet(barValues, "Distance cm")
+        // add a lot of colors
+        val colors = ArrayList<Int>()
+        if(random<=120){
+            colors.add(Color.RED)
+        }else{
+            colors.add(R.color.purple_toolbar)
+        }
+        //for (c in ColorTemplate.COLORFUL_COLORS) colors.add(c)
+        colors.add(R.color.purple_toolbar)
+        barDataSet.colors = colors
+        barDataSet.valueTextColor = Color.BLACK
+        barDataSet.valueTextSize = 15f
 
-    private fun getDataSet(): IBarDataSet? {
-        val dataSets = ArrayList<Any>()
-        val valueSet1 = ArrayList<Any>()
-        val v1e1 = BarEntry(110.000f, 0.0f) // Jan
-        valueSet1.add(v1e1)
-        val v1e2 = BarEntry(40.000f, 1.0f) // Feb
-        valueSet1.add(v1e2)
-        val v1e3 = BarEntry(60.000f, 2.0f) // Mar
-        valueSet1.add(v1e3)
-        val v1e4 = BarEntry(30.000f, 3.0f) // Apr
-        valueSet1.add(v1e4)
-        val v1e5 = BarEntry(90.000f, 4.0f) // May
-        valueSet1.add(v1e5)
-        val v1e6 = BarEntry(100.000f, 5.0f) // Jun
-        valueSet1.add(v1e6)
-        val valueSet2 = ArrayList<Any>()
-        val v2e1 = BarEntry(150.000f, 0.0f) // Jan
-        valueSet2.add(v2e1)
-        val v2e2 = BarEntry(90.000f, 1.0f) // Feb
-        valueSet2.add(v2e2)
-        val v2e3 = BarEntry(120.000f, 2.0f) // Mar
-        valueSet2.add(v2e3)
-        val v2e4 = BarEntry(60.000f, 3.0f) // Apr
-        valueSet2.add(v2e4)
-        val v2e5 = BarEntry(20.000f, 4.0f) // May
-        valueSet2.add(v2e5)
-        val v2e6 = BarEntry(80.000f, 5.0f) // Jun
-        valueSet2.add(v2e6)
-        val barDataSet1 = BarDataSet(valueSet1 as List<BarEntry>, "Brand 1")
-        barDataSet1.color = Color.rgb(0, 155, 0)
-        val barDataSet2 = BarDataSet(valueSet2 as List<BarEntry>, "Brand 2")
-        barDataSet2.setColors(*ColorTemplate.COLORFUL_COLORS)
-        //dataSets = ArrayList()
-        dataSets.add(barDataSet1)
-        dataSets.add(barDataSet2)
-        return dataSets as IBarDataSet?
+        val barData: BarData = BarData()
+        barData.barWidth = 10f
+        barData.addDataSet(barDataSet)
+        binding.chartBar.data = barData
+        binding.chartBar.invalidate()
+
     }
 
     // You must implements your logic to get data using OrmLite
